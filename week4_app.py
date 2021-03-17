545699542e08413d20febd999ca26bee3b46b532
unknown <b790718@gmail.com>
2021-03-17 22:30:41 +0800

    second commit

--- week4_app.py
+++ week4_app.py
@@ -1,18 +1,28 @@
 from flask import Flask 
 from flask import request
 from flask import render_template
+from flask import redirect
 app=Flask(__name__,static_folder="static",static_url_path="/")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/member")
+@app.route("/member/")
 def member():
     return render_template("member.html")
 
-@app.route("/error")
+@app.route("/error/")
 def error():
     return render_template("error.html")
 
-app.run(port=3000) 
+@app.route("/signin")
+def signin():
+    user=request.args.get("user")
+    password=request.args.get("password")
+    if user=="test" and password=="test":
+        return redirect("/member")
+    else:
+        return redirect("/error")
+
+app.run(debug=True,port=3000) 
